64fbb0fe58282667c22a015046cd303e41dc7d94
erschm240 <schmitze608@gmail.com>
2026-01-13 16:19:43 -0700

    Added imports and variables, and express middleware code to complete Routing Assignment "Serve Static Files."

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
-// Import express using ESM syntax
+// Imports
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// Create an instance of the user's name
-const name = process.env.NAME;
-
+/*
+* Declare Important Variables 
+*/
 // Define the port number the server will listen on
 const PORT = process.env.port || 3000;
 
+// Define filename and dirname for locating module files
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/*
+* Setup Express Server
+*/
 // Create an instance of an Express application
 const app = express();
 
+/*
+* Configure Express Middleware
+*/
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*
+* Declare Routes
+*/
 // Define a route handler for the root URL ('/')
 app.get('/', (req, res) => {
     res.send(`Welcome, ${name}!`);
