432dd06e77bfe7bd39a0d4485becd5754f902761
erschm240 <schmitze608@gmail.com>
2026-01-14 20:56:08 -0700

    feat: changed the static html pages to ejs files and updated the routes, created partial templates for the header and footer, added a student page for the exercise, changed background color to purple

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import path from 'path';
 * Declare Important Variables 
 */
 // Define the port number the server will listen on
+const NODE_ENV = process.env.NOVE_ENV || 'production';
 const PORT = process.env.port || 3000;
 
 // Define filename and dirname for locating module files
@@ -25,19 +26,37 @@ const app = express();
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express wehere to find the templates
+app.set('views', path.join(__dirname, 'src/views'));
+
 /*
 * Declare Routes
 */
 app.get('/',(req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+    const title = 'Welcome Home';
+    res.render('home', {title});
 })
 
 app.get('/about',(req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+    const title = 'About Me';
+    res.render('about', {title});
 })
 
 app.get('/products',(req, res) => {
-    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+    const title = 'Our Products';
+    res.render('products', {title});
+})
+
+app.get('/student', (req, res) => {
+    const title = 'Student Information';
+    const name = 'Emma';
+    const id = '48370592';
+    const email = 'stu25083@byui.edu';
+    const address =  '123 College Ave';
+    res.render('student', {title, name, id, email, address});
 })
 
 // Start the server and listen on the specified port
